1779870c3889ad9d8119b59985ffe993d4876add
dev adi <aditya.anand156@gmail.com>
2018-04-08 17:33:55 +0530

    Initial commit

---
+++ src/com/imadianand/Main.java
@@ -0,0 +1,17 @@
+package com.imadianand;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Q2>
+	//convert a given number of pounds to kilograms
+        //1. Create a variable to store number of pounds
+        //2. calculate the number of kilograms for the number above store in a variable.
+        //3. Printout the result.
+
+        // notes: 1 pound is equal to 0.45359237 kg.
+        int pounds = 50;
+        double kg= pounds*0.45359237d ;
+        System.out.println("kg =" +kg);
+    }
+}
